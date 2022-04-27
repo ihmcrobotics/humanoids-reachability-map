900e43da205ce73726641d57a947c5ea7e9efb47
Sylvain Bertrand <sbertrand@ihmc.org>
2022-04-27 14:40:24 -0500

    Converted the reachability sims to SCS2.

--- src/main/java/us/ihmc/reachabilityMap/AtlasReachabilitySphereMapSimulation.java
+++ src/main/java/us/ihmc/reachabilityMap/AtlasReachabilitySphereMapSimulation.java
@@ -5,78 +5,83 @@ import java.io.IOException;
 import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.avatar.drcRobot.RobotTarget;
-import us.ihmc.avatar.jointAnglesWriter.JointAnglesWriter;
 import us.ihmc.avatar.reachabilityMap.ReachabilitySphereMapCalculator;
 import us.ihmc.commons.FormattingTools;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
-import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.mecano.multiBodySystem.interfaces.OneDoFJointBasics;
+import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
 import us.ihmc.mecano.tools.MultiBodySystemTools;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
-import us.ihmc.robotics.partNames.LimbName;
 import us.ihmc.robotics.robotSide.RobotSide;
-import us.ihmc.simulationconstructionset.FloatingRootJointRobot;
-import us.ihmc.simulationconstructionset.SimulationConstructionSet;
-import us.ihmc.simulationconstructionset.SimulationConstructionSetParameters;
-import us.ihmc.simulationconstructionset.util.RobotController;
-import us.ihmc.yoVariables.registry.YoRegistry;
+import us.ihmc.scs2.definition.controller.ControllerOutput;
+import us.ihmc.scs2.definition.robot.RobotDefinition;
+import us.ihmc.scs2.sessionVisualizer.jfx.SessionVisualizer;
+import us.ihmc.scs2.sessionVisualizer.jfx.SessionVisualizerControls;
+import us.ihmc.scs2.simulation.SimulationSession;
+import us.ihmc.scs2.simulation.SimulationSessionControls;
+import us.ihmc.scs2.simulation.robot.Robot;
 
 public class AtlasReachabilitySphereMapSimulation
 {
    public AtlasReachabilitySphereMapSimulation() throws IOException
    {
       AtlasRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ, RobotTarget.SCS, false);
-      FullHumanoidRobotModel fullRobotModel = robotModel.createFullRobotModel();
-      FloatingRootJointRobot sdfRobot = robotModel.createHumanoidFloatingRootJointRobot(false);
-      final JointAnglesWriter jointAnglesWriter = new JointAnglesWriter(sdfRobot, fullRobotModel);
-
-      SimulationConstructionSetParameters parameters = new SimulationConstructionSetParameters(true, 16000);
-      SimulationConstructionSet scs = new SimulationConstructionSet(sdfRobot, parameters);
-      scs.setGroundVisible(false);
-
-      OneDoFJointBasics[] armJoints = MultiBodySystemTools.createOneDoFJointPath(fullRobotModel.getChest(), fullRobotModel.getHand(RobotSide.LEFT));
-      ReachabilitySphereMapCalculator calculator = new ReachabilitySphereMapCalculator(armJoints, scs);
-      sdfRobot.setController(calculator);
-      sdfRobot.setController(new RobotController()
-      {
-         @Override
-         public void initialize()
-         {
-         }
-         
-         @Override
-         public YoRegistry getYoRegistry()
-         {
-            return new YoRegistry("dummy");
-         }
-         
-         @Override
-         public void doControl()
-         {
-            jointAnglesWriter.updateRobotConfigurationBasedOnFullRobotModel();
-         }
-      });
-      calculator.setControlFramePose(fullRobotModel.getHandControlFrame(RobotSide.LEFT)
-                                                   .getTransformToDesiredFrame(fullRobotModel.getHand(RobotSide.LEFT).getBodyFixedFrame()));
-      FramePose3D palmCenter = new FramePose3D(fullRobotModel.getHandControlFrame(RobotSide.LEFT));
-      palmCenter.changeFrame(fullRobotModel.getEndEffector(RobotSide.LEFT, LimbName.ARM).getBodyFixedFrame());
-      RigidBodyTransform transformFromPalmCenterToHandBodyFixedFrame = new RigidBodyTransform();
-      palmCenter.get(transformFromPalmCenterToHandBodyFixedFrame);
-      //      reachabilitySphereMapCalculator.setTransformFromControlFrameToEndEffectorBodyFixedFrame(transformFromPalmCenterToHandBodyFixedFrame);
       String robotName = FormattingTools.underscoredToCamelCase(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ.toString(), true);
+
+      RobotDefinition robotDefinition = robotModel.getRobotDefinition();
+      robotDefinition.ignoreAllJoints();
+      FullHumanoidRobotModel fullRobotModel = robotModel.createFullRobotModel();
+
+      RigidBodyBasics base = fullRobotModel.getChest();
+      RigidBodyBasics endEffector = fullRobotModel.getHand(RobotSide.LEFT);
+      ReferenceFrame controlFrame = fullRobotModel.getHandControlFrame(RobotSide.LEFT);
+
+      SimulationSession session = new SimulationSession("Reachability Analysis - Atlas");
+      session.initializeBufferSize(16000);
+      Robot robot = session.addRobot(robotDefinition);
+      ReachabilitySphereMapCalculator calculator = setupCalculator(robotName, base, endEffector, controlFrame, robot.getControllerOutput());
+
+      robot.addController(calculator);
+      session.addYoGraphicDefinition(calculator.getYoGraphicVisuals());
+
+      SessionVisualizerControls guiControls = SessionVisualizer.startSessionVisualizer(session);
+      guiControls.setCameraFocusPosition(calculator.getGridFramePose().getX(), calculator.getGridFramePose().getY(), calculator.getGridFramePose().getZ());
+      guiControls.setCameraOrientation(Math.toRadians(15.0), Math.toRadians(170.0), 0.0);
+      calculator.setStaticVisualConsumer(guiControls::addStaticVisual);
+
+      SimulationSessionControls simControls = session.getSimulationSessionControls();
+      simControls.addExternalTerminalCondition(calculator::isDone);
+      simControls.simulate(Integer.MAX_VALUE);
+   }
+
+   private ReachabilitySphereMapCalculator setupCalculator(String robotName,
+                                                           RigidBodyBasics base,
+                                                           RigidBodyBasics endEffector,
+                                                           ReferenceFrame controlFrame,
+                                                           ControllerOutput controllerOutput)
+         throws IOException
+   {
+      OneDoFJointBasics[] armJoints = MultiBodySystemTools.createOneDoFJointPath(base, endEffector);
+      ReachabilitySphereMapCalculator calculator = new ReachabilitySphereMapCalculator(armJoints, controllerOutput);
+
+      calculator.setControlFramePose(controlFrame.getTransformToDesiredFrame(endEffector.getBodyFixedFrame()));
+      //      FramePose3D palmCenter = new FramePose3D(controlFrame);
+      //      palmCenter.changeFrame(endEffector.getBodyFixedFrame());
+      //      RigidBodyTransform transformFromPalmCenterToHandBodyFixedFrame = new RigidBodyTransform();
+      //      palmCenter.get(transformFromPalmCenterToHandBodyFixedFrame);
+      //      calculator.setTransformFromControlFrameToEndEffectorBodyFixedFrame(transformFromPalmCenterToHandBodyFixedFrame);
+
       calculator.setupCalculatorToRecordInFile(robotName, getClass());
-      //      calculator.setGridParameters(25, 0.075, 50, 1);
-      calculator.setGridParameters(5, 0.075, 5, 1);
+            calculator.setGridParameters(25, 0.075, 50, 1);
+//      calculator.setGridParameters(5, 0.075, 5, 1);
       calculator.setAngularSelection(false, true, true);
 
       FramePose3D gridFramePose = new FramePose3D(ReferenceFrame.getWorldFrame(), armJoints[0].getFrameBeforeJoint().getTransformToWorldFrame());
       gridFramePose.appendTranslation(calculator.getGridSizeInMeters() / 2.5, 0.15, -0.20);
       calculator.setGridFramePose(gridFramePose);
 
-      scs.setSimulateDoneCriterion(() -> calculator.isDone());
-      scs.startOnAThread();
-      scs.simulate();
+      return calculator;
    }
 
    public static void main(String[] args) throws IOException
--- src/main/java/us/ihmc/reachabilityMap/ValkyrieReachabilitySphereMapSimulation.java
+++ src/main/java/us/ihmc/reachabilityMap/ValkyrieReachabilitySphereMapSimulation.java
@@ -3,71 +3,78 @@ package us.ihmc.reachabilityMap;
 import java.io.IOException;
 
 import us.ihmc.avatar.drcRobot.RobotTarget;
-import us.ihmc.avatar.jointAnglesWriter.JointAnglesWriter;
 import us.ihmc.avatar.reachabilityMap.ReachabilitySphereMapCalculator;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.mecano.multiBodySystem.interfaces.OneDoFJointBasics;
+import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
 import us.ihmc.mecano.tools.MultiBodySystemTools;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
 import us.ihmc.robotics.robotSide.RobotSide;
-import us.ihmc.simulationconstructionset.FloatingRootJointRobot;
-import us.ihmc.simulationconstructionset.SimulationConstructionSet;
-import us.ihmc.simulationconstructionset.SimulationConstructionSetParameters;
-import us.ihmc.simulationconstructionset.util.RobotController;
+import us.ihmc.scs2.definition.controller.ControllerOutput;
+import us.ihmc.scs2.definition.robot.RobotDefinition;
+import us.ihmc.scs2.sessionVisualizer.jfx.SessionVisualizer;
+import us.ihmc.scs2.sessionVisualizer.jfx.SessionVisualizerControls;
+import us.ihmc.scs2.simulation.SimulationSession;
+import us.ihmc.scs2.simulation.SimulationSessionControls;
+import us.ihmc.scs2.simulation.robot.Robot;
 import us.ihmc.valkyrie.ValkyrieRobotModel;
-import us.ihmc.yoVariables.registry.YoRegistry;
 
 public class ValkyrieReachabilitySphereMapSimulation
 {
    public ValkyrieReachabilitySphereMapSimulation() throws IOException
    {
       ValkyrieRobotModel robotModel = new ValkyrieRobotModel(RobotTarget.SCS);
+      String robotName = "Valkyrie";
+
+      RobotDefinition robotDefinition = robotModel.getRobotDefinition();
+      robotDefinition.ignoreAllJoints();
+
       FullHumanoidRobotModel fullRobotModel = robotModel.createFullRobotModel();
-      FloatingRootJointRobot sdfRobot = robotModel.createHumanoidFloatingRootJointRobot(false);
-      final JointAnglesWriter jointAnglesWriter = new JointAnglesWriter(sdfRobot, fullRobotModel);
-
-      SimulationConstructionSetParameters parameters = new SimulationConstructionSetParameters(true, 16000);
-      SimulationConstructionSet scs = new SimulationConstructionSet(sdfRobot, parameters);
-      scs.setGroundVisible(false);
-
-      OneDoFJointBasics[] armJoints = MultiBodySystemTools.createOneDoFJointPath(fullRobotModel.getChest(), fullRobotModel.getHand(RobotSide.LEFT));
-      ReachabilitySphereMapCalculator calculator = new ReachabilitySphereMapCalculator(armJoints, scs);
-      sdfRobot.setController(calculator);
-      sdfRobot.setController(new RobotController()
-      {
-         @Override
-         public void initialize()
-         {
-         }
-
-         @Override
-         public YoRegistry getYoRegistry()
-         {
-            return new YoRegistry("dummy");
-         }
-
-         @Override
-         public void doControl()
-         {
-            jointAnglesWriter.updateRobotConfigurationBasedOnFullRobotModel();
-         }
-      });
-      calculator.setupCalculatorToRecordInFile("Valkyrie", getClass());
+      RigidBodyBasics base = fullRobotModel.getChest();
+      RigidBodyBasics endEffector = fullRobotModel.getHand(RobotSide.LEFT);
       RigidBodyTransform controlFrameToWristTransform = new RigidBodyTransform();
       controlFrameToWristTransform.getTranslation().set(0.025, 0.07, 0.0);
-      calculator.setControlFramePose(controlFrameToWristTransform);
-      calculator.setAngularSelection(false, true, true);
+
+      SimulationSession session = new SimulationSession("Reachability Analysis - Atlas");
+      session.initializeBufferSize(16000);
+      Robot robot = session.addRobot(robotDefinition);
+      ReachabilitySphereMapCalculator calculator = setupCalculator(robotName, base, endEffector, controlFrameToWristTransform, robot.getControllerOutput());
+
+      robot.addController(calculator);
+      session.addYoGraphicDefinition(calculator.getYoGraphicVisuals());
+
+      SessionVisualizerControls guiControls = SessionVisualizer.startSessionVisualizer(session);
+      guiControls.setCameraFocusPosition(calculator.getGridFramePose().getX(), calculator.getGridFramePose().getY(), calculator.getGridFramePose().getZ());
+      guiControls.setCameraOrientation(Math.toRadians(15.0), Math.toRadians(170.0), 0.0);
+      calculator.setStaticVisualConsumer(guiControls::addStaticVisual);
+
+      SimulationSessionControls simControls = session.getSimulationSessionControls();
+      simControls.addExternalTerminalCondition(calculator::isDone);
+      simControls.simulate(Integer.MAX_VALUE);
+   }
+
+   private ReachabilitySphereMapCalculator setupCalculator(String robotName,
+                                                           RigidBodyBasics base,
+                                                           RigidBodyBasics endEffector,
+                                                           RigidBodyTransform controlFrameTransform,
+                                                           ControllerOutput controllerOutput)
+         throws IOException
+   {
+      OneDoFJointBasics[] armJoints = MultiBodySystemTools.createOneDoFJointPath(base, endEffector);
+      ReachabilitySphereMapCalculator calculator = new ReachabilitySphereMapCalculator(armJoints, controllerOutput);
+
+      calculator.setControlFramePose(controlFrameTransform);
+      calculator.setupCalculatorToRecordInFile(robotName, getClass());
       calculator.setGridParameters(40, 0.05, 50, 1);
+      calculator.setAngularSelection(false, true, true);
 
       FramePose3D gridFramePose = new FramePose3D(ReferenceFrame.getWorldFrame(), armJoints[0].getFrameBeforeJoint().getTransformToWorldFrame());
       gridFramePose.appendTranslation(calculator.getGridSizeInMeters() / 2.5, 0.2, 0.0);
       calculator.setGridFramePose(gridFramePose);
 
-      scs.setSimulateDoneCriterion(() -> calculator.isDone());
-      scs.startOnAThread();
-      scs.simulate();
+      return calculator;
    }
 
    public static void main(String[] args) throws IOException
